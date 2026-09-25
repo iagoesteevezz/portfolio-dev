645f74dce6bdc9bef4a1761203c1738a36132d7c
Iago Estévez <noa.arqnatura@gmail.com>
2026-09-25 11:03:47 +0100

    Serve from custom domain iagoestevez.com

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  base: '/portfolio-dev/',
+  base: '/',
   build: {
     rollupOptions: {
       input: {
